e23ccc9dd82ec917ac1c60b837417bb6e8c64476
qaacademy <qaacademy@gmail.com>
2022-03-03 19:46:44 -0300

    Ex10 - Easy

--- src/main/java/qaacademy_exercicios_junit/easy/Exercicio10_easy.java
+++ src/main/java/qaacademy_exercicios_junit/easy/Exercicio10_easy.java
@@ -5,8 +5,6 @@ public class Exercicio10_2_easy {
 
     public double calcularValorJuros(double valorInvestimento) {
         double valorJuros = 0.0, taxaJuros = 0.05, ano = 1;
-        valorInvestimento = 1000.00;
-
         while (ano <= 10) {
             valorJuros = valorJuros + (valorInvestimento * taxaJuros);
             ano = ano + 1;
---
+++ src/test/java/TestExercicio10_easy.java
@@ -0,0 +1,27 @@
+import java.text.DecimalFormat;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import qaacademy_exercicios_junit.easy.Exercicio10_2_easy;
+
+public class TestExercicio10_easy {
+
+    static Exercicio10_2_easy ex10;
+    static DecimalFormat decimalFormat;
+
+    @BeforeClass
+    public static void before() {
+        ex10 = new Exercicio10_2_easy();
+        decimalFormat = new DecimalFormat("#,###.00");
+    }
+
+    @Test
+    public void testeCalculaValorJuros() {
+        String valorEsperado = "666,50";
+        String valorAltual = decimalFormat.format(ex10.calcularValorJuros(1333.00));
+        Assert.assertEquals(valorEsperado, valorAltual);
+    }
+
+}

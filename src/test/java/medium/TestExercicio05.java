a94e16582d89d45c5fabcd928577382440035066
qaacademy <qaacademy@gmail.com>
2022-03-03 21:34:35 -0300

    Teste ex05 - medium

---
+++ src/test/java/medium/TestExercicio05.java
@@ -0,0 +1,25 @@
+package medium;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import qaacademy_exercicios_junit.medium.Exercicio05_medium;
+
+
+public class TestExercicio05 {
+
+    static Exercicio05_medium ex5;
+
+    @BeforeClass
+    public static void before() {
+        ex5 = new Exercicio05_medium();
+    }
+
+    @Test
+    public void testeTabuada() {
+       Assert.assertEquals(1, ex5.verificaMenorNumero()); 
+    }
+
+    
+}

5b7904dca7dac8c4f948ff62d5531e06a22158a0
qaacademy <qaacademy@gmail.com>
2022-03-03 21:03:07 -0300

    Teste Ex2 - Medium

---
+++ src/test/java/medium/TestExercicio02.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.text.DecimalFormat;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import qaacademy_exercicios_junit.medium.Exercicio02_medium;
+
+public class TestExercicio02 {
+    static DecimalFormat decimalFormat;
+
+       
+    static Exercicio02_medium ex2;
+
+    @BeforeClass
+    public static void before() {
+        ex2 = new Exercicio02_medium();
+        decimalFormat = new DecimalFormat("#,###.00");
+    }
+
+    @Test
+    public void testeCalculaJurosCompostos(){
+        String valorEsperado = "628,89";
+        String valorAtual =  decimalFormat.format(ex2.calculaInvestimentoJurosCompostos(1000.00));
+
+        Assert.assertEquals(valorEsperado, valorAtual);
+
+    }
+    @Test
+    public void testeCalculaValorTotal(){
+        String valorEsperado = "1.628,89";
+        String valorAtual =  decimalFormat.format(ex2.calculaValorTotalInvestimento(1000.00, ex2.calculaInvestimentoJurosCompostos(1000.00)));
+        Assert.assertEquals(valorEsperado, valorAtual);
+
+    }
+    
+}
